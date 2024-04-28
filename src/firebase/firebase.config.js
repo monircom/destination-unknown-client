5cc07afd1cce5435537ff9ac0c278817988c5157
monircom <monircom@gmail.com>
2024-04-28 11:13:26 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDwYqinT9e4NBsfoFPIit3TK4SQsBohNUg",
+//   authDomain: "destination-unknown-ed9f8.firebaseapp.com",
+//   projectId: "destination-unknown-ed9f8",
+//   storageBucket: "destination-unknown-ed9f8.appspot.com",
+//   messagingSenderId: "790989533003",
+//   appId: "1:790989533003:web:ccf0e36ada75e5a73f02ce"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
