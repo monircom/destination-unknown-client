a72acf14d797cbde504335b0d1447e55d18987f2
monircom <monircom@gmail.com>
2024-04-29 12:25:39 +0600

    React Awesome Reveal added

--- src/components/SwiperPage.jsx
+++ src/components/SwiperPage.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from "react";
+
+import { Fade } from "react-awesome-reveal";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -31,33 +33,47 @@ const SwiperPage = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/1.jpg)] ">        
+          <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/1.jpg)] "> 
+          <Fade>  
             <h1 className="text-5xl ">Beautiful home with modern amenities.</h1>
+            </Fade>     
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/2.jpg)] ">        
+        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/2.jpg)] ">     
+        <Fade>    
             <h1 className="text-5xl ">Cozy townhouse in a family-friendly neighborhood.</h1>
+          </Fade>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/3.jpg)] ">        
+        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/3.jpg)] ">   
+             
+        <Fade>   
             <h1 className="text-5xl ">Modern apartment with stunning city views.</h1>
+            </Fade>
           </div>
         </SwiperSlide>
         <SwiperSlide>
         <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/4.jpg)] ">        
+        <Fade>
             <h1 className="text-5xl ">Comfortable student housing near campus.</h1>
+            </Fade>
+
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/5.jpg)] ">        
+        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/5.jpg)] ">  
+        <Fade>
             <h1 className="text-5xl ">Retirement community with various amenities.</h1>
+            </Fade>  
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/6.jpg)] ">        
+        <div className="slide bg-[linear-gradient(180deg,rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(./assets/6.jpg)] ">      
+        <Fade>
             <h1 className="text-5xl ">Charming vacation rental by the beach.</h1>
+            </Fade>  
           </div>
         </SwiperSlide>
       </Swiper>
