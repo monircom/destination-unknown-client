ac77b74c4557e91ab3376e5898c46e067b0ad0fb
monircom <monircom@gmail.com>
2024-04-30 11:50:35 +0600

    swal added

--- src/components/AddTouristsSpot.jsx
+++ src/components/AddTouristsSpot.jsx
@@ -1,26 +1,58 @@
+import Swal from 'sweetalert2'
 const AddTouristsSpot = () => {
+
+    const handleAddSpot = event =>{
+        event.preventDefault();
+        const form = event.target;
+
+        const imageURL = form.imageURL.value;
+        const spotName = form.spotName.value;
+        const country = form.country.value;
+        const location = form.location.value;
+        const description = form.description.value;
+        const averageCost = form.averageCost.value;
+        const seasonality = form.seasonality.value;
+        const travelTime = form.travelTime.value;
+        const totalVisitorsPerYear = form.totalVisitorsPerYear.value;
+
+        const newTouristSpot = {imageURL, spotName, country, location, description, averageCost, seasonality, travelTime, totalVisitorsPerYear};
+        console.log(newTouristSpot);
+
+        fetch('http://localhost:5000/tourist-spot',{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newTouristSpot)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Tourist Spot Added Successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                  })
+            }
+        })
+
+    }
   return (
-    <div>
-      <h1>Add Tourists Spot</h1>
-      {/*  Image
-      tourists_spot_name
-c. country_Name
-d. location
-e. short description
-f. average_cost
-g. seasonality - like summer, winter
-h. travel_time => like- 7 days
-i. totaVisitorsPerYear => like- 10000 */}
-      <form>
+    <div>     
+
+      <form onSubmit={handleAddSpot}>
         <div className="md:max-w-[80%] mx-auto">
           <div className="flex">
             <div className="form-control w-full mx-6">
               <label className="label">
-                <span className="label-text">Image URL</span>
+                <span className="label-text text-2xl">Image URL</span>
               </label>
               <label className="input-group">
                 <input
                   type="text"
+                  name="imageURL"
                   placeholder="Image URL"
                   className="input input-bordered w-full"
                 />
@@ -33,11 +65,12 @@ i. totaVisitorsPerYear => like- 10000 */}
           <div className="flex flex-col md:flex-row ">
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">Tourists spot name</span>
+                <span className="label-text text-2xl">Tourists spot name</span>
               </label>
               <label className="input-group">
                 <input
                   type="text"
+                  name="spotName"
                   placeholder="Tourists spot name"
                   className="input input-bordered w-full"
                 />
@@ -46,10 +79,10 @@ i. totaVisitorsPerYear => like- 10000 */}
 
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">Country Name</span>
+                <span className="label-text text-2xl">Country Name</span>
               </label>
               <label className="input-group">
-                <select className="select select-primary w-full md:max-w-sm">
+                <select name="country" className="select select-primary w-full md:max-w-sm">
                   <option disabled selected>
                     Select Country
                   </option>
@@ -69,12 +102,13 @@ i. totaVisitorsPerYear => like- 10000 */}
           <div className="flex flex-col md:flex-row ">
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">Location</span>
+                <span className="label-text text-2xl">Location</span>
               </label>
               <label className="input-group">
                 <input
                   type="text"
-                  placeholder="Tourists spot name"
+                  name="location"
+                  placeholder="Location"
                   className="input input-bordered w-full"
                 />
               </label>
@@ -82,12 +116,13 @@ i. totaVisitorsPerYear => like- 10000 */}
 
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">short description</span>
+                <span className="label-text text-2xl">Short Description</span>
               </label>
               <label className="input-group">
               <input
                   type="text"
-                  placeholder="Tourists spot name"
+                  name="description"
+                  placeholder="short description"
                   className="input input-bordered w-full"
                 />
               </label>
@@ -98,12 +133,13 @@ i. totaVisitorsPerYear => like- 10000 */}
            <div className="flex flex-col md:flex-row ">
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">average_cost</span>
+                <span className="label-text text-2xl">Average Cost</span>
               </label>
               <label className="input-group">
                 <input
                   type="text"
-                  placeholder="Tourists spot name"
+                  name="averageCost"
+                  placeholder="$"
                   className="input input-bordered w-full"
                 />
               </label>
@@ -111,11 +147,12 @@ i. totaVisitorsPerYear => like- 10000 */}
 
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">seasonality</span>
+                <span className="label-text text-2xl">seasonality</span>
               </label>
               <label className="input-group">
               <input
                   type="text"
+                  name="seasonality"
                   placeholder="summer, winter ..."
                   className="input input-bordered w-full"
                 />
@@ -127,11 +164,12 @@ i. totaVisitorsPerYear => like- 10000 */}
           <div className="flex flex-col md:flex-row ">
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">travel_time</span>
+                <span className="label-text text-2xl">Travel Time</span>
               </label>
               <label className="input-group">
                 <input
                   type="text"
+                  name="travelTime"
                   placeholder="7 days"
                   className="input input-bordered w-full"
                 />
@@ -140,18 +178,22 @@ i. totaVisitorsPerYear => like- 10000 */}
 
             <div className="form-control md:w-1/2 mx-6">
               <label className="label">
-                <span className="label-text">totaVisitorsPerYear</span>
+                <span className="label-text text-2xl">Total Visitors Per Year</span>
               </label>
               <label className="input-group">
               <input
                   type="text"
+                  name="totalVisitorsPerYear"
                   placeholder="10000"
                   className="input input-bordered w-full"
                 />
               </label>
             </div>
           </div>
-        </div>
+          <div className="mx-6 mt-5 mb-10">
+          <input type="submit" value="Add Tourist Spot" className="btn btn-block font-bold text-2xl" />
+          </div>
+        </div>        
       </form>
     </div>
   );
