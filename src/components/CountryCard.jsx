894c453dd349a18759a7d4c23353ae61bae0161d
monircom <monircom@gmail.com>
2024-04-30 22:14:20 +0600

    Bug Fix

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,10 +1,11 @@
 
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const CountryCard = ({ country }) => {
     return (
-        <div>        
+        <div>
+          <NavLink to={`/country-wise-card/${country.country_name}`}>        
     <div className="card w-full  h-full bg-base-100 shadow-xl border-2">
           <figure className="rounded-xl bg-gray-300 m-5">
             <img src={country.image} alt="Country" className="w-[100%] h-full rounded-xl" />
@@ -16,12 +17,13 @@ const CountryCard = ({ country }) => {
            
             <hr className="my-6 border-t-2 border-dashed"></hr>     
             <div className="">
-              <div>
+              {/* <div>
               <Link to={`/country-wise-card/${country.country_name}`} state={country.country_name}>  <button className="btn btn-primary w-full">View {country.country_name}</button>  </Link>         
-              </div>           
+              </div>            */}
           </div>
           </div>          
-        </div>        
+        </div>
+        </NavLink>        
       </div>
     );
 };
